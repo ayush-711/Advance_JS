8670730b5b7ed26e8a8eedba662cc99bb8b1eb26
Ayush Kumar <ayush18111998@gmail.com>
2026-01-11 18:16:24 +0530

    object declaration and syntax

---
+++ script.js
@@ -0,0 +1,16 @@
+let user = {
+    name: 'Ayush',
+    age: 21,
+    email: 'ayush@gmail.com',
+    location: 'HAHAHAHA',
+    blogs : ['hello friends' , 'chai peelo']
+
+}
+
+console.log(user);
+console.log(user.name);
+console.log(user.blogs[1]);
+
+user.name = 'okayush';
+
+console.log(user.name);
