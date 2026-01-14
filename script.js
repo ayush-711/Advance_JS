98acf949570520898c03fa0ae1397e9ba4ab93d4
Ayush Kumar <ayush18111998@gmail.com>
2026-01-14 16:22:52 +0530

    promises syntax

--- script.js
+++ script.js
@@ -3,14 +3,36 @@ let user = {
     age: 21,
     email: 'ayush@gmail.com',
     location: 'HAHAHAHA',
-    blogs : ['hello friends' , 'chai peelo']
+    blogs: ['hello friends', 'chai peelo'],
+    login() {
+        console.log("the user logged in!")
+    },
+    logout() {
+        console.log("the user logged out!")
+    } ,
+    logBlogs() {
+        console.log("the user has writte the following blogs :")
+        this.blogs.forEach(blog => { console.log(blog); });
+        
+    }
 
-}
+};
 
-console.log(user);
-console.log(user.name);
-console.log(user.blogs[1]);
+// console.log(user);
+// console.log(user.name);
+// console.log(user.blogs[1]);
+// user.name = 'okayush';
+// console.log(user.name);
 
-user.name = 'okayush';
+// Methods on Objects
 
-console.log(user.name);
+user.login();
+user.logout();
+
+user.logBlogs();
+
+
+
+
+
+
